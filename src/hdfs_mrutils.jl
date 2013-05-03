fcb8f4418c61072d55b87732766a0cae932c866d
tan <tanmaykm@gmail.com>
2013-05-03 14:18:57 +0530

    changes for 32bit machines

--- src/hdfs_mrutils.jl
+++ src/hdfs_mrutils.jl
@@ -40,7 +40,7 @@ function hdfs_find_rec_csv(jc::HdfsJobCtx, rec_sep, col_sep, max_rec_bytes::Int,
             local is_interesting::Bool = bchk(jc)
             if(is_interesting)
                 #jc.rec = split(ascii(rdr.cv[jc.next_rec_pos:end_pos]), col_sep)
-                jc.rec = split(bytestring(sub(rdr.cv, jc.next_rec_pos:end_pos)), col_sep)
+                jc.rec = split(bytestring(sub(rdr.cv, int(jc.next_rec_pos):int(end_pos))), col_sep)
                 is_interesting = rchk(jc)
             end
             jc.next_rec_pos = end_pos+2
