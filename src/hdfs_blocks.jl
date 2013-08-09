58ffd491ce056d48c645f8d5a6883c7f8017e107
tan <tanmaykm@gmail.com>
2013-08-09 16:33:41 +0530

    handle case where worker ip is set to loopback ip

--- src/hdfs_blocks.jl
+++ src/hdfs_blocks.jl
@@ -6,9 +6,15 @@ export  Block
 
 function Block(f::HdfsURL)
     worker_ids = workers()
-    worker_ips = map(x->getaddrinfo(string(isa(x, LocalProcess)?getipaddr():x.host)), map(x->Base.worker_from_id(x), worker_ids))
+    localip = getipaddr()
+    loopbackip = IPv4(127,0,0,1)
+    worker_ips = map(x->getaddrinfo(string(isa(x, LocalProcess)?localip:x.host)), map(x->Base.worker_from_id(x), worker_ids))
 
     block_dist = hdfs_blocks(f, 1, 0, true)
+    for iplist in block_dist
+        # add loopback interface for localip
+        contains(iplist, localip) && !contains(iplist, loopbackip) && push!(iplist, loopbackip)
+    end
     block_wrkr_ids = map(ips->worker_ids[findin(worker_ips, ips)], block_dist)
     filestat = stat(f)
     block_sz = filestat.block_sz
